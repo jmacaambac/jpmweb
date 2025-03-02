98fe08ba30daef16667c2b87e067f3faebc62652
Jade <jade.macaambac@gmail.com>
2025-03-03 03:08:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,53 @@
 javascript
 const CACHE_NAME = 'portfolio-v1';
 const WEBSITE_URL = 'https://jmacaambac.github.io/myprofile/';
-
+const urlsToCache = [
+  WEBSITE_URL,
+  'index.html',
+  'manifest.json',
+  
+];
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        return cache.add(WEBSITE_URL);
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.error('Cache failed:', error);
       })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request)
-      .catch(() => {
-        return caches.match(event.request)
+    caches.match(event.request)
+      .then((response) => {
+       
+        if (response) {
+          return response;
+        }
+      
+        const fetchRequest = event.request.clone();
+        return fetch(fetchRequest)
           .then((response) => {
-            if (response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-            if (event.request.url === WEBSITE_URL) {
-              return caches.match(WEBSITE_URL);
-            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
           });
+      })
+    .catch(() => {
+        
+        if (event.request.url === WEBSITE_URL) {
+          return caches.match(WEBSITE_URL);
+        }
       })
   );
 });
@@ -34,6 +58,7 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
